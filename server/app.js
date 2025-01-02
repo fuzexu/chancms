c020a9b518a8b22e0e107129771e23c83a47834e
qly_fuzexu <fuzexu@qluanyun.com>
2025-01-02 17:23:42 +0800

    初始

---
+++ server/app.js
@@ -0,0 +1,6 @@
+﻿const Chanjs = require("chanjs");
+const chan = new Chanjs();
+chan.start();
+chan.run((port) => {
+  console.log(`ChanCMS is running on ${port}`);
+});
---
+++ server/app/config/config.base.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+const ROOT_PATH = process.cwd();
+const APP_PATH = path.join(ROOT_PATH, "app");
+const config = {};
+config.appRoot = APP_PATH;
+config.version = "v3.0.9";
+config.appName = "ChanCMS";
+config.port = "81";
+config.versionTime = "2024-10-01";
+config.author = "yanyutao";
+config.authorEmail = "867528315@qq.com";
+config.authorWechat = "yanyutao2014";
+config.JSON_LIMIT = "100kb";
+
+config.static = [{
+  prefix: "/public/",
+  dir: ["app/public"],
+  maxAge: 0,
+}];
+
+config.modules = ['api','common','web'];
+config.plugins = ['plus-wechat'];
+
+module.exports = config;
